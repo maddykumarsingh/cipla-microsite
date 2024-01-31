8f85c2eba72a930b536bdc290278ca853509f3a5
Subrat Chowdhary <subratchowdhary@outlook.com>
2024-01-31 18:10:25 +0530

    Responsive Login

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli('localhost', 'root', 'root', 'cipla');
+$conn = new mysqli('localhost', 'root', 'sarthak@123', 'cipla');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
--- login.php
+++ login.php
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             background-image: url('./dist/images/login-min.gif'); /* Replace with the actual path to your image */
             background-size: cover;
             background-position: center;
-            background-repeat: no-repeat;
+            background-repeat: repeat;
             height: 100%;
             display: flex;
             flex-direction: column;
@@ -61,11 +61,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <div class="background-image">
-        <div class="top-right text-white text-5xl font-extrabold justify-end mr-10 ">Cipla</div>
-        <div class="flex text-6xl font-medium md:text-5xl sm:text-4xl p-2 font-arial text-white justify-center ">You are born to do great things</div>
+        <div class="flex justify-end items-end p-4"><img src="./dist/images/Cipla_White.png"/></div>
+        <div class="flex text-6xl font-medium md:text-5xl sm:text-5xl sm:mt-40 sm:p-6 p-2 font-arial text-white justify-center ">You are born to do great things</div>
        
-        <div class="flex text-7xl  lg:text-7xl md:6xl sm:5xl p-4 font-arial text-white justify-center items-center">
-                <div class="flex">Welcome to HR Offsite 2024</div> 
+        <div class="flex text-6xl lg:text-7xl md:6xl sm:6xl p-4 font-arial text-white justify-center items-center">
+                Welcome to HR Offsite 2024 
         </div>
         
         <div class="flex p-4 justify-center mb-4 sm:mb-8">
@@ -75,14 +75,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
         <form method="post" action="login.php" class="flex flex-col">
             <div class=" flex flex-col">
-                <div class="flex justify-center m-2">
-                    <input type="text" name="email" class="flex p-4 w-2/3 rounded-lg text-lg text-center placeholder:text-2xl " placeholder="Enter Email">
+                <div class="flex justify-center m-2 sm:m-4">
+                    <input type="text" name="email" class="flex p-4 md:w-1/3 sm:w-full rounded-lg text-lg text-center placeholder:text-2xl " placeholder="Enter Email">
                 </div>
-                <div class="flex justify-center  m-2">
-                    <input type="password" name="password" class="flex p-4 w-2/3 rounded-lg text-lg text-center placeholder:text-2xl " placeholder="Enter Password">
+                <div class="flex justify-center  m-2 sm:m-4">
+                    <input type="password" name="password" class="flex p-4 w-1/4 md:w-1/3 sm:w-1/2  rounded-lg text-lg text-center placeholder:text-2xl " placeholder="Enter Password">
                 </div>
-                <div class="flex justify-center m-2">
-                    <button class=" flex flex-col font-semibold bg-blue-900 p-2 px-4 text-white text-xl rounded-xl justify-center items-center">
+                <div class="flex justify-center m-2 sm:m-4">
+                    <button class=" flex flex-col font-semibold bg-blue-600 p-2 px-4 text-white text-xl rounded-xl justify-center items-center w-1/3 sm:w-1/3 mt-4">
                         <p class="text-3xl font-arial font-thin">LOGIN</p>
                         <p class="font-normal font-arial px-4"> to let your greatness unfold</p>
                     </button>
