7113d6533ba51a233f1262901e14ca8c0e5a5b51
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2024-01-31 15:46:39 +0530

    Login with Password only

--- login.php
+++ login.php
@@ -6,9 +6,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $query = "SELECT * FROM users WHERE email = ? AND password = ?";
+    $query = "SELECT * FROM users WHERE password = ?";
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("ss", $email, $password);
+    $stmt->bind_param("s",$password);
     $stmt->execute();
     $result = $stmt->get_result();
 
