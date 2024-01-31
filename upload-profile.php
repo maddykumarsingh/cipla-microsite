ee2e4a73a0a07508871d9b63ad4b6ed8c8aa60ff
Nitin Singh <singh.nitinkumar.nitin1995@gmail.com>
2024-01-31 08:53:23 +0530

    Changed Login Background

---
+++ upload-profile.php
@@ -0,0 +1,13 @@
+<?php
+$uploadDirectory = __DIR__ . '/uploads/user/profile-image/';
+$timestamp = time();
+$extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
+$uploadedFile = $uploadDirectory . $timestamp . '.' . $extension;
+
+if (move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadedFile)) {
+    echo json_encode(['filename' =>basename($uploadedFile)]);
+} else {
+    http_response_code(500);
+    echo json_encode(['error' => 'Failed to upload file']);
+}
+?>
